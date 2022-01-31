981eb5e94ae3534064eed51ee5d33d6ad6d793b8
Matías Fernández <mfernandez.1992@icloud.com>
2022-01-31 20:49:12 -0300

    Clases

--- 25 - Métodos estáticos, getters y setters/app.js
+++ 25 - Métodos estáticos, getters y setters/app.js
@@ -1,4 +1,4 @@
-/* Métodos estáticos - se pueden ejecutar sin la necesidad de instanciar la clase */
+/* Métodos estáticos - se pueden ejecutar sin la necesidad de instanciar la clase, en JavaScript no existen las clases privadas, las clases privadas son las que tienen métodos y argumentos que solamente se pueden utilizar dentro del cuerpo de la clase */
 
 class Animal{
     // El constructor es un método especial que se ejecuta en el momento de instanciar la clase
@@ -35,11 +35,22 @@ class Perro extends Animal{
         console.log('Guauu Guauu!');
     }
 
+    /* Declaro métodos estáticos con static */
     static queEres(){
         console.log('Los perros somos considerados los mejores amigos del hombre');
     }
 
     // Los setters y getters son métodos especiales que nos permiten establecer y obtener los valores de atributos de nuestra clase
+    // Método obtenedor que obtiene el valor de la propiedad
+    get getRaza(){
+        return this.raza;
+    }
+
+    // Método establecedor que modifica dicha propiedad
+    set setRaza(){
+        this.raza = raza;
+    }
+
 }
 
 // Titan hereda las propiedades de perro y mostrará el método ladrar del constructor perro
@@ -55,3 +66,6 @@ titan.sonar();
 titan.saludar();
 pope.sonar();
 pope.saludar();
+console.log(titan.getRaza); // Da null xq no contiene valor y es el que le asigné por defecto
+titan.setRaza = 'Cimarrón'; // Asigno la raza mediante el set
+console.log(titan.getRaza); // Ahora devuelve la raza Cimarrón
