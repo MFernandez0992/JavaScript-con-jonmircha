cc869783f12397be688c5da380480a146c718b07
Matías Fernández <mfernandez.1992@icloud.com>
2022-01-31 22:09:29 -0300

    console

---
+++ 26 - Objeto Console/app.js
@@ -0,0 +1,17 @@
+/* Objeto Console */
+
+console.log(console);
+console.error('Esto es un error');
+console.warn('Esto es un aviso');
+console.info('Esto es un mensaje informativo');
+console.log('Un registro de lo que ha pasado en nuestra aplicación');
+
+let nombre = 'Matías',
+    apellido = 'Fernández',
+    edad = 29;
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(nombre, apellido, edad);
+console.log(`Hola mi nombre es ${nombre} ${apellido} y tengo ${edad} años`);
