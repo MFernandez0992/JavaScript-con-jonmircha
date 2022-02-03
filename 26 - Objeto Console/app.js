383d0d65faa4b95828cbbcfce001a22c10bdbc02
Matías Fernández <mfernandez.1992@icloud.com>
2022-02-02 21:04:14 -0300

    console

--- 26 - Objeto Console/app.js
+++ 26 - Objeto Console/app.js
@@ -14,4 +14,55 @@ console.log(nombre);
 console.log(apellido);
 console.log(edad);
 console.log(nombre, apellido, edad);
-console.log(`Hola mi nombre es ${nombre} ${apellido} y tengo ${edad} años`);
+console.log(`Hola mi nombre es ${nombre} ${apellido} y tengo ${edad} años.`);
+console.clear();
+
+console.log(window);
+console.log(document);
+console.dir(window);
+console.dir(document);
+
+// Crear grupos de elementos en la consola
+console.group('Cursos de @jonmirha en YouTube');
+console.log('Curso de JavaScript');
+console.log('Curso de Node.js');
+console.log('Curso de PWAs');
+console.groupEnd();
+
+console.log(console);
+console.table(Object.entries(console).sort())
+
+const numeros = [1, 2, 3, 4, 5];
+const vocales = ["a", "b", "c", "d", "e"];
+
+console.table(numeros, vocales);
+
+const perro = {
+    nombre: 'Titán',
+    raza: 'Cimarrón',
+    color: 'Atigrado'
+}
+
+console.table(perro);
+
+// Cronometrar cuánto tarda en ejecutarse el código
+console.time('Cuánto tiempo tarda mi código');
+
+const arreglo = Array(1000000);
+for(let i = 0; i < arreglo.length; i++){
+    arreglo[i] = i;
+}
+
+console.timeEnd('Cuánto tiempo tarda mi código'); // El mensaje de time(...) y de timeEnd(...) debe ser igual para que funcione
+
+// console.count() - cuenta cuantos console.log ejecuta el código, en este caso el for
+for(let i = 0; i <= 100; i++){
+    console.count('Código for');
+    console.log(i);
+}
+
+// console.assert(condición) - evalúa por consola una condición y muestra un msj mostrando qué esperaba en caso de que no se cumpla
+let x = 1;
+let y = 2;
+let pruebaXY = 'Se espera que X siempre sea menor que Y';
+console.assert(X < y, [x, y, pruebaXY])
